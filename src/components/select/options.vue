2c66fbd9d6a72f6e4d43603b835f107e8d62a7a7
xo <654720416@qq.com>
2019-09-09 20:43:40 +0800

    feat: select init

---
+++ src/components/select/options.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="xo-options">
+    <ul>
+      <li
+      v-for="item in options"
+      :key="item.value"
+      :value="item.value"
+      :label="item.label"
+      >{{item.label}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    options: {
+      type: Array,
+      default: () => {
+        return [];
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.xo-options {
+  color: #606266;
+  position: absolute;
+  z-index: 2222;
+  background-color: #fff;
+  min-width: 180px;
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
+  border: 1px solid #dcdfe6;
+  border-radius: 4px;
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 10px 15px;
+    li {
+      padding: 5px 10px;
+      text-align: start;
+    }
+  }
+  ul::before {
+    content: '';
+    width: 0;
+    height: 0;
+    border: 6px solid #dcdfe6;
+    border-left-color: transparent;
+    border-right-color: transparent;
+    border-top-color: transparent;
+    border-bottom-color: #dcdfe6;
+    position: absolute;
+    left: 15px;
+    top: -13px;
+  }
+  ul::after {
+    content: '';
+    width: 0;
+    height: 0;
+    border: 6px solid #fff;
+    border-left-color: transparent;
+    border-right-color: transparent;
+    border-top-color: transparent;
+    border-bottom-color: #fff;
+    position: absolute;
+    top: -12px;
+    left: 15px;
+  }
+}
+</style>
+
--- src/components/select/select.vue
+++ src/components/select/select.vue
@@ -1,155 +1,62 @@
 <template>
   <div class="xo-select">
-    <div class="xo-select-input">
-      <xoInput
-      select
-      @click="inputClick"
-      placeholder="请选择"
-      :selectInput="multiple ? '' : select">
-        <template v-slot:suffix>
-          <i class="icon-triangle-down select-triangle" :class="{triangleDown: showOption}"></i>
-        </template>
-      </xoInput>
-    </div>
-    <div class="xo-select-option" v-show="showOption">
-      <i class="icon-option"></i>
-      <div class="xo-select-option_content">
-        <ul>
-          <li
-          v-for="(item, index) in options"
-          :key="index"
-          @mouseenter="hoverRow(index)"
-          @mouseleave="hoverRow(index)"
-          @click="optionClick(index)"
-          :class="{
-          hover: multiple ? 
-          hover.indexOf(index) > -1 : hover == index
-          }"
-          >
-            <span>{{item.label}}</span>
-            <!-- <span v-else>
-              <slot></slot>
-            </span> -->
-          </li>
-        </ul>
-      </div>
+    <xo-input
+    :placeholder="placeholder"
+    @input-click="inputClick">
+      <template v-slot:suffix>
+        <i v-if="showOptions" class="icon-triangle-down select-down"></i>
+        <i v-else class="icon-triangle-up select-up"></i>
+      </template>
+    </xo-input>
+    <div v-if="showOptions">
+      <xo-options :options="options"></xo-options>
     </div>
   </div>
 </template>
 
 <script>
-import xoInput from '../input/input';
-import '@/common/icon.less';
-
+import xoInput from '@/components/input/index.js';
+import xoOptions from './options';
 export default {
-  name: 'xoSelect',
-  components: {
-    xoInput,
-  },
   data() {
     return {
-      select: '',
-      showOption: false,
-      hover: -1,
-    };
+      showOptions: false,
+    }
   },
   props: {
-    options: Array,
-    multiple: {
-      type: Boolean,
-      default: false
+    placeholder: {
+      type: String,
+      default: '请选择'
+    },
+    options: {
+      type: Array,
+      default: () => {
+        return [];
+      }
     }
+  },
+  components: {
+    xoInput,
+    xoOptions
   },
   methods: {
     inputClick(val) {
-      if(this.hover != -1) {
-        this.showOption = true;
-      }else {
-        this.showOption = val ? !this.showOption : val;
-      }
-    },
-    optionClick(index) {
-      if(this.multiple) {
-        this.select = this.select == '' ? [] : this.select;
-        const flag = this.select.indexOf(index);
-        if(flag > -1) {
-          this.select.splice(flag,1);
-        }else {
-          this.select.push(index);
-        }
-      }else {
-        this.select = this.select == index ? -1 : index;
-      }
-    },
-    hoverRow(index) {
-      this.hover = this.hover == index ? -1 : index;
+      this.showOptions = val;
     }
-  },
-};
+  }
+}
 </script>
 
 <style lang="less" scoped>
-@fontColor: #515a6e;
 .xo-select {
-  width: 230px;
-  position: relative;
-  .xo-select-input {
-    padding: 10px 0;
-    input {
-      -webkit-appearance:none;
-      -moz-appearance: none;
-      border: 1px solid #e8eaec;
-      width: 230px;
-      height: 22px;
-      border-radius: 7px;
-    }
-    input:focus {
-      outline: none;
-      border-color: #409EFF;
-    }
-    .select-triangle {
-      top: 13px;
-    }
-    .triangleDown {
-      transform: rotate(180deg);
-      top: 8px;
-    }
+  .xo-input {
+    cursor: pointer;
   }
-  .xo-select-option {
-    position: absolute;
-    top: 45px;
-    z-index: 222;
-    &>i {
-      top: -11px;
-      left: 25px;
-      color: #e8eaec;
-    }
-    .xo-select-option_content {
-      min-width: 180px;
-      border: 1px solid #e8eaec;
-      border-radius: 7px;
-      box-shadow: 0 0 10px #e8eaec;
-      background-color: #FFF;
-      &>ul {
-        list-style: none;
-        padding-left: 0;
-        display: flex;
-        flex-direction: column;
-        padding: 10px 0;
-        margin: 0;
-        &>li {
-          color: @fontColor;
-          text-align: left;
-          padding: 0 18px;
-          font-size: 14px;
-          height: 30px;
-          line-height: 30px;
-        }
-        .hover {
-          background: #f5f7fa;
-        }
-      }
-    }
+  .select-down {
+    top: 10px !important;
+  }
+  .select-up {
+    top: -10px !important;
   }
 }
 </style>
