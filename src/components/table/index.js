0c2281af5b9ff67c9c2e56260d10ec2bcc70f78f
xo <654720416@qq.com>
2019-09-11 13:49:38 +0800

    fix: 表格使用说明更新

--- src/components/table/index.js
+++ src/components/table/index.js
@@ -5,9 +5,10 @@
  * 表格内竖直线：verticalLine
  * 斑马纹：stripe，斑马纹颜色（需设置stripe有效）：stripeColor
  * 选中行高亮：highlightRow，选中事件：row-click(row, column)
- * 展开子行：data中对需展开的行增加children属性，可指定展开箭头出现的列（默认第一列）：arrowPosition
+ * 展开子行：data中对需展开的行增加children属性，在表头array中可指定展开箭头出现的列（默认第一列）：arrowPosition
  *         可自定义展开行的表头：childcolumns，默认为columns
  * 自定义展开内容：expand，同时对需展开的列，设置rowExpand
+ * 固定列：在表头array中需固定的列指定fixed: end/start
  */
 
 import xoTable from './table.vue';
