b29589c82f8aa9b87698f2504009a4d6d2c46cf3
Hellottxo <654720416@qq.com>
2019-10-15 19:50:16 +0800

    blog init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  title: 'XO-UI',
+  description: 'A Vue.js 2.0 UI Toolkit',
+  themeConfig: {
+    nav: [
+      { text: 'Github', link: 'https://github.com/Hellottxo' },
+    ],
+    sidebar: [
+      {
+        title: '开发者指南',
+        collapsable: false,
+        children: [
+          '/Development/quick-start',
+          '/Development/built-in-transition',
+        ],
+      },
+      {
+        title: '组件',
+        collapsable: false,
+        children: [
+          '/Components/button',
+          '/Components/tree',
+          '/Components/input',
+          '/Components/select',
+          '/Components/time-picker',
+          '/Components/date-picker',
+          '/Components/table',
+        ],
+      },
+    ],
+    lastUpdated: 'Last Updated',
+  },
+};
