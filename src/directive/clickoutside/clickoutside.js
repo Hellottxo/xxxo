1bd504efb3a276ebce15cd1492cb22f37ae5facc
Hellottxo <654720416@qq.com>
2019-11-14 11:26:59 +0800

    fix: 优化v-clickoutside

--- src/directive/clickoutside/clickoutside.js
+++ src/directive/clickoutside/clickoutside.js
@@ -1,19 +1,60 @@
 /* eslint-disable */
+import Vue from 'vue';
+
+const nodeList = [];
+const ctx = '@@clickoutsideContext';
+
+let startClick;
+let seed = 0;
+
+!Vue.prototype.$isServer && document.addEventListener('mousedown', e => (startClick = e));
+!Vue.prototype.$isServer && document.addEventListener('mouseup', e => {
+  nodeList.forEach(node => node[ctx].documentHandler(e, startClick));
+});
+
 export default {
-  bind(el, binding) {
-    function documentHandler(e) {
-      if (el.contains(e.target)) {
-        return false;
-      }
-      if (binding.expression) {
-        binding.value(e);
+  bind(el, binding, vnode) {
+    nodeList.push(el);
+    const id = seed++;
+    el[ctx] = {
+      id,
+      documentHandler: createDocumentHandler(el, binding, vnode),
+      methodName: binding.expression,
+      bindingFn: binding.value
+    };
+
+    function createDocumentHandler(el, binding, vnode) {
+      return function(mouseup = {}, mousedown = {}) {
+        if (!vnode ||
+          !vnode.context ||
+          !mouseup.target ||
+          !mousedown.target ||
+          el.contains(mouseup.target) ||
+          el.contains(mousedown.target) ||
+          el === mouseup.target ||
+          (vnode.context.popperElm &&
+          (vnode.context.popperElm.contains(mouseup.target) ||
+          vnode.context.popperElm.contains(mousedown.target)))) return;
+    
+        if (binding.expression &&
+          el[ctx].methodName &&
+          vnode.context[el[ctx].methodName]) {
+          vnode.context[el[ctx].methodName]();
+        } else {
+          el[ctx].bindingFn && el[ctx].bindingFn();
+        }
+      };
+    }
+  },  
+  unbind(el) {
+    let len = nodeList.length;
+  
+    for (let i = 0; i < len; i++) {
+      if (nodeList[i][ctx].id === el[ctx].id) {
+        nodeList.splice(i, 1);
+        break;
       }
     }
-    el._clickoutside = documentHandler;
-    document.addEventListener('click', documentHandler)
-  },
-  unbind (el) {
-    document.removeEventListener('click', el._clickoutside);
-    delete el._clickoutside;
-  }
+    delete el[ctx];
+  }  
 }
